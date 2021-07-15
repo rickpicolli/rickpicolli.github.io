b324e9ba5e77676d771147d2f3801370f6c7e958
Ricardo Alexandre Pinheiro Picolli <rickpicolli@Mac.local>
2021-07-14 17:36:44 -0700

    stating creating components

---
+++ YoutubeReactRMDB/react-rmdb/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Components
+import Header from './components/Header';
+
+//styles
+import { GlobalStyle } from './GlobalStyle';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Start here.
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
---
+++ YoutubeReactRMDB/react-rmdb/src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+//config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+
+//components
+
+
+//Hook
+
+
+//Image
+import NoImage from '../images/no_image.jpg';
+
+const Home = () => {
+    return <div>Home Page</div>
+}
